170b7cc0b480b46a104d03f1c70b145452b14fb5
MyUsername <rosanio.c@gmail.com>
2024-11-05 14:43:45 +0100

    Snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,13 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+// FOREACH
+names.forEach(item => {
+    console.log(item);
+})
+
+// FOREOF
+for (let item of names) {
+    console.log(item);
+}
