4c1f53e4b58a59de359678fb047d8208f2921f4f
MyUsername <rosanio.c@gmail.com>
2024-11-05 14:53:23 +0100

    Snack 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,21 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+console.log('Array originale:');
+console.log(numbers);
+
+// SOLUZIONE 1
+const plusOne = numbers.map((number) => {
+    return number += 1;
+});
+
+console.log('Soluzione 1:');
+console.log(plusOne);
+
+
+// SOLUZIONE 2 (COMPATTA)
+const newPlusOne = numbers.map(number => number += 1);
+
+console.log('Soluzione 2 compatta:');
+console.log(newPlusOne);
